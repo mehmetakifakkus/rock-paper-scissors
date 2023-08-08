2eefd57de876fc8c69f23660662432a843b7dd7c
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-08 12:59:43 +0300

    Add Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <div className="flex flex-row justify-between px-[18px] py-[14px] border-2 border-gray-500 w-[590px] h-[126px] rounded-xl">
+      <Image
+        src="/images/logo.svg"
+        alt="logo"
+        className="w-[142px] h-[90px] pt-[6px] pl-[6px]"
+        width={142}
+        height={90}
+      />
+      <div className="w-[128px] p-3 flex flex-col items-center bg-white rounded-lg">
+        <span className="text-sm font-barlow tracking-widest text-blue-700">
+          SCORE
+        </span>
+        <span className="text-[52px] leading-[52px] font-barlow font-bold text-gray-600">
+          12
+        </span>
+      </div>
+    </div>
+  );
+}

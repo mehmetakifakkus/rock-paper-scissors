87f5a45d9436d885952bc6af03ea530b9c162c0e
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-09 13:03:48 +0300

    Redirect mainpage to /step1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  useRouter().push("/step1");
+  return <></>;
 }

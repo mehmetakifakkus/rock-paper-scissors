d11b06f780a5ebdd2cba8e1927501c2ab87f65c0
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-08-11 12:06:41 +0300

    Add animation to rules button

--- app/components/RulesButton.tsx
+++ app/components/RulesButton.tsx
@@ -10,7 +10,7 @@ export default function RulesButton({ setOpened }: Props) {
       onClick={() => {
         setOpened(true);
       }}
-      className="fixed bottom-8 right-8 w-[132px] border px-6 py-2 font-barlow text-center border-gray-200 rounded-md bg-transparent cursor-pointer"
+      className="fixed bottom-8 right-8 w-[132px] border px-6 py-2 font-barlow text-center border-gray-200 rounded-md bg-transparent cursor-pointer hover:rotate-[20deg] ease-jiggle2 duration-300"
     >
       <span className="text-white text leading-[8px] tracking-[2px]">
         RULES
